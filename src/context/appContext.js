0bfe79ac8f5fb10a18062ad068fa3ca2aab4469f
Maduz0097 <aymperera@gmail.com>
2022-05-12 07:56:56 +0530

    deploy v2

--- src/context/appContext.js
+++ src/context/appContext.js
@@ -73,7 +73,7 @@ const AppProvider = ({ children }) => {
 
   // axios
   const authFetch = axios.create({
-    baseURL: "/api/v1",
+    baseURL: "https://salespal-dev.herokuapp.com/api/v1",
   });
   // request
 
